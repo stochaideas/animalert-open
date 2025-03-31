4cd35ecb5cef3193669ae30703635681fac6f0ae
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2025-03-31 21:26:21 +0300

    refactor: replace button element with Button component and update styles

--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { Button } from "~/components/ui/button";
 
 import { api } from "~/trpc/react";
 
@@ -37,13 +38,12 @@ export function LatestPost() {
           onChange={(e) => setName(e.target.value)}
           className="w-full rounded-full bg-white/10 px-4 py-2 text-white"
         />
-        <button
+        <Button
           type="submit"
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
           disabled={createPost.isPending}
         >
           {createPost.isPending ? "Submitting..." : "Submit"}
-        </button>
+        </Button>
       </form>
     </div>
   );
