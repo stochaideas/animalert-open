77147b6284ae4faadcb88300b9c6162a7498e8d3
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2025-04-02 19:33:43 +0300

    feat: add NEXT_PUBLIC_GOOGLE_MAPS_API_KEY to environment variables

--- src/env.js
+++ src/env.js
@@ -20,6 +20,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: z.string(),
   },
 
   /**
@@ -29,6 +30,9 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
+      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
