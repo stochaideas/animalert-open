1af3dbfd5d4d17a3f7b8e98e11ef354583777a6d
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2025-04-03 20:31:17 +0300

    feat: implement geolocation for Google Map and enhance loading state

--- src/app/_components/map.tsx
+++ src/app/_components/map.tsx
@@ -6,23 +6,30 @@ import {
   Map,
   type MapMouseEvent,
 } from "@vis.gl/react-google-maps";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { env } from "~/env";
 
 export function GoogleMap() {
-  const [position, setPosition] = useState<google.maps.LatLngLiteral>({
-    lat: 53.54992,
-    lng: 10.00678,
-  });
+  const [position, setPosition] = useState<
+    google.maps.LatLngLiteral | undefined
+  >();
 
-  // const position = { lat: 53.54992, lng: 10.00678 };
   const apiKey = env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
   const mapId = env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID as string;
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        const { latitude, longitude } = coords;
+        setPosition({ lat: latitude, lng: longitude });
+      });
+    }
+  }, []);
 
   const onClickMap = (event: MapMouseEvent) => {
     const lat = event.detail.latLng?.lat;
     const lng = event.detail.latLng?.lng;
-    if (lat && lng) {
+    if (lat && lng && position) {
       setPosition({ lat, lng });
     }
   };
@@ -30,14 +37,20 @@ export function GoogleMap() {
   return (
     <APIProvider apiKey={apiKey}>
       <div style={{ width: "100%", height: "100vh" }}>
-        <Map
-          onClick={onClickMap}
-          defaultCenter={position}
-          defaultZoom={13}
-          mapId={mapId}
-        >
-          <AdvancedMarker position={position} />
-        </Map>
+        {position ? (
+          <Map
+            onClick={onClickMap}
+            defaultCenter={position}
+            defaultZoom={13}
+            mapId={mapId}
+          >
+            <AdvancedMarker position={position} />
+          </Map>
+        ) : (
+          <div className="flex h-full w-full items-center justify-center">
+            <p className="text-xl font-semibold">Loading...</p>
+          </div>
+        )}
       </div>
     </APIProvider>
   );
