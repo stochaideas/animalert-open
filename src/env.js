2df230067029a6c487f593c18b43b3e7118deea8
Bere Darius <47079143+BereDarius@users.noreply.github.com>
2025-05-09 19:37:55 +0300

    Update env.js

--- src/env.js
+++ src/env.js
@@ -12,8 +12,8 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
 
-    AWS_ACCESS_KEY_ID: z.string(),
-    AWS_SECRET_ACCESS_KEY: z.string(),
+    AWS_ACCESS_KEY_ID: z?.string(),
+    AWS_SECRET_ACCESS_KEY: z?.string(),
     AWS_REGION: z.string(),
     AWS_S3_BUCKET_NAME: z.string(),
   },
