70c078019c74211626b2b02f9a49a2aa5dd363cd
Mali Attila Szabolcs <56229692+mszab0310@users.noreply.github.com>
2025-06-12 14:46:49 +0300

    Increase max video size and fix comparision error in validation (#26)

--- src/constants/file-constants.ts
+++ src/constants/file-constants.ts
@@ -1,5 +1,5 @@
 export const IMAGE_MAX_SIZE = 10 * 1024 * 1024; // 10MB
-export const VIDEO_MAX_SIZE = 10 * 1024 * 1024; // 10MB
+export const VIDEO_MAX_SIZE = 200 * 1024 * 1024; // 200MB
 
 export const ACCEPTED_IMAGE_TYPES = [
   "image/png",
