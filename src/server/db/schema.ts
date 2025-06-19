cd9418da11dfd86d9c8b7425230d5208f263a86c
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2025-06-19 20:31:50 +0300

    Add feedback table and rating type; update feedback dialog for submission logic

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 export * from "~/server/api/modules/user/user.schema";
 export * from "~/server/api/modules/report/report.schema";
 export * from "~/server/api/modules/feedback/feedback.schema";
-export * from "~/server/api/modules/complaint-template/complaint_template.schema"
-export * from "~/server/api/modules/complaint/complaint.schema"
+export * from "~/server/api/modules/complaint-template/complaint_template.schema";
+export * from "~/server/api/modules/complaint/complaint.schema";
 
 export * from "~/server/db/relations";
