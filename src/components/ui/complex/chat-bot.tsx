394e9a987eeea86f796f43ab864e1c1bfd0a1c77
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2025-06-24 19:47:41 +0300

    Refactor ChatBot component to replace text link with a button for editing answers and update conditional rendering for review mode

--- src/components/ui/complex/chat-bot.tsx
+++ src/components/ui/complex/chat-bot.tsx
@@ -154,15 +154,17 @@ export default function ChatBot({
                   : answered.answer}
               </div>
               <div className="flex flex-row justify-end">
-                <span
-                  className="text-secondary text-body-small mb-8 cursor-pointer font-bold"
+                <Button
+                  variant="primary"
+                  size="sm"
+                  // className="text-secondary text-body-small mb-8 cursor-pointer font-bold"
                   onClick={() => {
                     setEditingIdx(idx);
                     setStep(idx);
                   }}
                 >
                   🔄 Modifică răspuns
-                </span>
+                </Button>
               </div>
             </div>
             <SVGUser width="32" height="32" />
@@ -250,7 +252,7 @@ export default function ChatBot({
                   </Button>
                 </form>
               )}
-              {answers.length > 1 && (
+              {answers.length > 1 && reviewMode && (
                 <Button
                   variant="tertiary"
                   className="mt-2"
